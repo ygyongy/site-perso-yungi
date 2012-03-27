777c16f7f02fc190da064740f4f24cfa5bff43ea
ygyongy <ygyongy@bc5032ef-df98-8373-71bf-a2679fe54973>
2012-03-27 22:36:48 +0000

    Mise a jour => form_interactif.php Ajout d'une image pour les message d'erreur du formulaire

--- trunk/test_ajax/form_interactif.php
+++ trunk/test_ajax/form_interactif.php
@@ -20,7 +20,7 @@
             <p>
                 <ul>
                     <li>Pseudo plus de 4 caractères</li>
-                    <li>MDP deux fois identiques</li>
+                    <li>MDP deux fois identiques et plus de 6 caractères</li>
                     <li>Une réponse secrète de plus de 4 caractères</li>
                 </ul>
             </p>
@@ -55,13 +55,18 @@
                     width: 190px;
                     outline: none;
                     padding: 2px;
-                    border: 1px #abadb3 solid;
+                }
+                
+                #TP_form form fieldset div select
+                {
+                    width: 196px;
                 }
                 
                 #TP_form form fieldset div input:focus, select:focus
                 {
                     border: 1px dodgerblue solid;
                     background-color: #D7D7D7;
+                    padding: 2px;
                 }
                 
                 .correct
@@ -81,8 +86,31 @@
                 
                 .incorrect:focus
                 {
-                    border: 1px dodgerblue solid;                    
-                }                
+                    border: 1px dodgerblue solid;
+                    padding: 2px;
+                }
+                
+                .tooltip
+                {
+                    display: inline-block;
+                    -moz-border-radius: 3px;
+                    -webkit-border-radius: 3px;
+                    border: darkred 1px solid;
+                    background: pink;
+                    padding: 3px;
+                    padding-left: 25px;
+                    margin-left: 10px;
+                    background-image: url('image/box-error.gif');
+                    background-repeat: no-repeat;
+                    background-position: 5px;
+                    color: darkred;
+                }
+                
+                #secret_question
+                {
+                    padding: 2px;
+                    margin-left: 212px;
+                }
             </style>
             
             <div id="TP_form">
@@ -97,11 +125,11 @@
                         <div>
                             <label for="password">Saisissez un Password: </label>
                             <input type="password" name="password" id="password" class="required" />
-                            <span class="tooltip">Le mot de passe doit correspondre au deuxième</span>
+                            <span class="tooltip">Le mot de passe doit faire plus de 6 caractères</span>
                             <br/>
                             <label for="password2">Re-saisissez votre Password: </label>
                             <input type="password" name="password2" id="password2" class="required" />
-                            <span class="tooltip">Le mot de passe doit correspondre au premier</span>
+                            <span class="tooltip">Les mots de passe doivent correspondre</span>
                         </div>
                         <div>
                             <label for="question">Choisissez une question secrète: </label>
@@ -112,15 +140,196 @@
                                 <option value="animal">De quel type était votre premier animal?</option>
                             </select>
                             <span class="tooltip">Vous devez au moins sélectionnez une question</span>
-                            <br/><br/>
+                            <br/>
+                                <span id="secret_question"></span>
+                            <br/>
                             <label for="answer">Saisissez votre réponse: </label>
                             <input type="text" name="answer" id="answer" class="required" />
                             <span class="tooltip">Vous devez répondre à la question avec un minimum de 4 caractères</span>
                         </div>
                         <div>
-                            <input type="button" value="créer le membre" name="create_user" id="create_user" />
-                            <input type="button" value="effacer le formulaire" name="reset_user" id="reset_user" />
+                            <input type="submit" value="créer le membre" name="create_user" id="create_user" />
+                            <input type="reset" value="effacer le formulaire" name="reset_user" id="reset_user" />
                         </div>
                     </fieldset>
                 </form>
+                
+                
+                
+                <script type="text/javascript">
+                    (function(){ //utilisation d'une fonction anonyme pour plus d'aisance
+                        function disableTooltips()
+                        {
+                            var spans = document.getElementsByTagName('span');
+                            var spansLength = spans.length;
+                            
+                            for(i = 0; i < spansLength; i++)
+                                {
+                                    if(spans[i].className === 'tooltip')
+                                        {
+                                            spans[i].style.display = 'none';
+                                        }
+                                }
+                        }
+                        
+                        function getTooltip(element)
+                        {
+                            var el = element;
+                            
+                            while(el)
+                                {
+                                    if(el.className === 'tooltip')
+                                        {
+                                            return el; // si la classe eset bien tooltip tu retournes l'élément
+                                        }
+                                    el = el.nextSibling; //ne pas oublier à la fin de la boucle de changer le statut de l'élément testé... sinon boucle sans fin
+                                }
+                                
+                             return false; //si l'élément n'as pas de frère il n'y a pas de tooltip existant
+                        }
+                        
+                        
+                        //Maintenant pour contrôler le formulaire je pense faire un objet JSON
+                        var check = {};
+                        
+                            check['login'] = function(){
+                                var login = document.getElementById('login');
+                                var value = login.value.length;
+                                
+                                var tooltipStyle = getTooltip(login).style;
+                                
+                                if(value <= 4)
+                                    {
+                                        login.className = 'incorrect';
+                                        tooltipStyle.display = 'inline-block';
+                                        return false;
+                                    }else{
+                                        login.className = 'correct';
+                                        tooltipStyle.display = 'none';
+                                        return true;
+                                    }
+                            };
+                            
+                            check['password'] = function(){
+                                var pwd1 = document.getElementById('password');
+                                var tooltipStyle = getTooltip(pwd1).style;
+                                var value = pwd1.value.length;
+                                
+                                if(value <= 6)
+                                    {
+                                        pwd1.className = 'incorrect';
+                                        tooltipStyle.display = 'inline-block';
+                                        return false;
+                                    }else{
+                                        pwd1.className = 'correct';
+                                        tooltipStyle.display = 'none';
+                                        return false;
+                                    }
+                            };
+                            
+                            check['password2'] = function(){
+                                var pwd1 = document.getElementById('password');
+                                var pwd2 = document.getElementById('password2');
+                                var tooltipStyle = getTooltip(pwd2).style;
+                                
+                                var pwd1Value = pwd1.value;
+                                var pwd2Value = pwd2.value;
+                                
+                                if(pwd1Value !== pwd2Value)
+                                    {
+                                        pwd2.className = 'incorrect';
+                                        tooltipStyle.display = 'inline-block';
+                                        return false;
+                                    }else{
+                                        pwd2.className = 'correct';
+                                        tooltipStyle.display = 'none';
+                                        return true;
+                                    }
+                            };
+                            
+                            check['question'] = function(){
+                                var question = document.getElementById('question');
+                                var tooltipStyle = getTooltip(question).style;
+                                var wrapper = document.getElementById('secret_question');
+                                
+                                if(question.options[question.selectedIndex].value !== 'none')
+                                    {
+                                        var txt = question.options[question.selectedIndex].innerHTML;
+                                        question.className = 'correct';
+                                        wrapper.style.display = 'inline-block';
+                                        wrapper.innerHTML = txt;
+                                        wrapper.className = 'correct';
+                                        tooltipStyle.display = 'none';
+                                        return true;
+                                    }else{
+                                        question.className = 'incorrect'
+                                        tooltipStyle.display = 'inline-block';
+                                        wrapper.style.display = 'none';
+                                        return false;
+                                    }
+                            };
+                            
+                            check['answer'] = function(){
+                                var answer = document.getElementById('answer');
+                                var tooltipStyle = getTooltip(answer).style;
+                                var value = answer.value.length;
+                                
+                                if(value <= 4)
+                                    {
+                                        answer.className = 'incorrect';
+                                        tooltipStyle.display = 'inline-block';
+                                        return false
+                                    }else{
+                                        answer.className = 'correct';
+                                        tooltipStyle.display = 'none';
+                                        return true;
+                                    }
+                            };
+                            // Fin de l'objet de contrôle du formulaire
+                            
+                            
+                            //Mise en place des événemnts
+                            (function(){
+                                //utilisation d'une fonction anonyme afin d'éviter les variables globales
+                                var myForm = document.getElementById('create_user_form');
+                                var inputs = myForm.getElementsByTagName('*');
+                                
+                                var inputsLength = inputs.length;
+                                
+                                for(i = 0; i < inputsLength; i++)
+                                    {
+                                        if(inputs[i].type == 'text' || inputs[i].type == 'password')
+                                            {
+                                                inputs[i].onkeyup = function(){
+                                                    check[this.id]();
+                                                };
+                                            }else if (inputs[i].type == 'select-one'){
+                                                inputs[i].onchange = function(){
+                                                    check[this.id]();
+                                                };
+                                            }
+                                    }
+                                    
+                                 myForm.onsubmit = function(){
+                                     var result = true;
+                                     
+                                     for(j in check) // le fameux foreach en JS
+                                         {
+                                             result = check[j] && result;
+                                         }
+                                         
+                                     if (result){
+                                         alert('Le Formulaire est bien rempli');
+                                     }
+                                     
+                                     return false;
+                                 }
+                            })();
+                        
+                        
+                        disableTooltips();
+                        testss = document.getElementById('answer');
+                        testss.onkeyup = check[testss.id];
+                    })();
+                </script>
             </div>
